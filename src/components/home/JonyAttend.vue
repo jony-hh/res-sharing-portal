f868a1758a65c4a778df5c4f9744b869935e99f7
Jony <jonyh0812@gmail.com>
2024-03-07 00:19:22 +0800

    fix: 修改倒计日功能

--- src/components/home/JonyAttend.vue
+++ src/components/home/JonyAttend.vue
@@ -1,7 +1,70 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const loading = ref(false)
+
+const calcResidue = (month: any, day: any) => {
+  console.log(sortedFestivals.value)
+  // 获取当前日期
+  let currentDate = new Date()
+
+  // 设置目标日期为5月4日
+  let targetDate = new Date(currentDate.getFullYear(), month - 1, day) // 月份从0开始，所以5月对应的是4
+  // 如果目标日期已经过去，则将目标日期设置为下一年的5月4日
+  if (currentDate > targetDate) {
+    targetDate.setFullYear(targetDate.getFullYear() + 1)
+  }
+  // 计算时间差
+  let timeDiff = targetDate.getTime() - currentDate.getTime()
+  // 将时间差转换为天数
+  let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
+  return daysDiff
+}
+
+const political_festival = ref([
+  {
+    name: '五四青年节',
+    month: 5,
+    day: 4,
+  },
+  {
+    name: '中国共产党成立纪念日',
+    month: 7,
+    day: 1,
+  },
+  {
+    name: '抗日战争胜利纪念日',
+    month: 7,
+    day: 3,
+  },
+  {
+    name: '国庆节',
+    month: 10,
+    day: 1,
+  },
+])
+
+const sortedFestivals = computed(() => {
+  const currentDate = new Date()
+  return political_festival.value
+    .map((festival) => ({
+      ...festival,
+      targetDate:
+        currentDate <
+        new Date(currentDate.getFullYear(), festival.month - 1, festival.day)
+          ? new Date(
+              currentDate.getFullYear(),
+              festival.month - 1,
+              festival.day,
+            )
+          : new Date(
+              currentDate.getFullYear() + 1,
+              festival.month - 1,
+              festival.day,
+            ),
+    }))
+    .sort((a: any, b: any) => a.targetDate - b.targetDate)
+})
 </script>
 
 <template>
@@ -9,17 +72,18 @@ const loading = ref(false)
     <v-card elevation="16" class="mx-auto j-card" max-width="280">
       <v-card-text>
         <div style="color: #6200ea">政党重要节日</div>
-        <p class="text-h6 text--primary">
-          距离五四青年节还剩
-          <span style="color: red">150</span>
+        <p class="text-h6 text--primary" v-if="sortedFestivals.length > 0">
+          距离【{{ sortedFestivals[0].name }}】还剩
+          <span style="color: red">
+            {{ calcResidue(sortedFestivals[0].month, sortedFestivals[0].day) }}
+          </span>
           天
         </p>
         <div class="text--primary">
-          中国共产党成立纪念日（7月1日）
-          <br />
-          国庆节（10月1日）
-          <br />
-          抗日战争胜利纪念日（9月3日）
+          <template v-for="festival in sortedFestivals">
+            {{ festival.name }}（{{ festival.month }}月{{ festival.day }}日）
+            <br />
+          </template>
         </div>
       </v-card-text>
       <v-card-actions style="position: relative">
@@ -39,7 +103,7 @@ const loading = ref(false)
           variant="flat"
           color="deep-purple-accent-4"
         >
-          签到
+          今日签到
         </v-btn>
       </v-card-actions>
     </v-card>
