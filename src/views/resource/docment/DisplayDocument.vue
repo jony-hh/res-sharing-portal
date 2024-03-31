2c4155507a2d7cc8f89a44507a326f868252515d
Jony <jonyh0812@gmail.com>
2024-03-31 18:50:35 +0800

    fix: 优化元素与文档页面，增加点赞收藏功能

--- src/views/resource/docment/DisplayDocument.vue
+++ src/views/resource/docment/DisplayDocument.vue
@@ -5,6 +5,7 @@ import { fetchSingDocumentResource } from '@/api/res'
 
 const docRes = ref<any>({
   id: 0,
+  name:'',
   type: '',
   typeUrl: '',
   tags: [],
@@ -28,10 +29,26 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div style="width: 100%; height: 100vh">
+  <div style="width: 100%; height: 85vh">
+    <!-- 头部信息-->
+    <div class="ma-2" style="border: 1px red solid;height: 48px">
+      <span style="line-height: 48px">毕 业 设 计（论 文）</span>
+      <span style="float: right">
+        <v-btn
+          color="#61666D"
+          icon="mdi-thumb-up"
+          variant="text"
+        ></v-btn>
+        <v-btn
+          color="#61666D"
+          icon="mdi-star"
+          variant="text"
+        ></v-btn>
+      </span>
+    </div>
     <!--定义显示策略-->
     <!--1、word、excel、ppt 使用 iframe 简单预览-->
-    <div v-if="docRes.type !== 'pdf'" style="width: 100%; height: 100vh">
+    <div v-if="docRes.type !== 'pdf'" style="width: 100%; height: 100%">
       <iframe
         :src="
           'https://view.officeapps.live.com/op/view.aspx?src=' +
@@ -43,7 +60,7 @@ onMounted(async () => {
       ></iframe>
     </div>
     <!-- 2、pdf 使用 pdf.js  -->
-    <div v-else id="pdf-container" style="width: 100%; height: 100vh">
+    <div v-else id="pdf-container" style="width: 100%; height: 100%">
       <iframe
         :src="docRes.document_url"
         width="100%"
--- src/views/resource/topic/detail/TopicElement.vue
+++ src/views/resource/topic/detail/TopicElement.vue
@@ -2,6 +2,7 @@
 import { LocationQueryValue, useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import { fetchSingTopicResource } from '@/api/res'
+import { useUserStore } from "@/store/user";
 
 interface topicType {
   id: number
@@ -11,15 +12,53 @@ interface topicType {
 }
 
 const router = useRouter()
+const userStore = useUserStore()
 const topicInfo = ref<topicType>({
   id: 0,
   backgroundUrl: '',
   title: '',
   content: '',
 })
+
+// 操作dto
+const operate = ref({
+  content_id: topicInfo.value.id,
+  user_id: userStore.loginUser.id,
+  content_type: 'course',
+})
+// 点赞与收藏数据
+const thumb_num = ref(0)
+const star_num = ref(0)
+// 按钮是否被点亮
+const thumb_flag = ref(false)
+const star_flag = ref(false)
+
 const getSingleTopicData = async (id: string | null | LocationQueryValue[]) => {
   const res = await fetchSingTopicResource(id)
   topicInfo.value = { ...res.data }
+}
+// 更新数据
+const thumb = async (operate: object) => {
+  //const res = await userThumb(operateDTO)
+  //thumb_num.value = res.data.thumb_num
+  if (!thumb_flag.value) {
+    thumb_num.value++
+    thumb_flag.value = true
+  } else {
+    thumb_num.value--
+    thumb_flag.value = false
+  }
+}
+const star = async (operate: object) => {
+  //const res = await userStar(operateDTO)
+  //star_num.value = res.data.star_num
+  if (!star_flag.value) {
+    star_num.value++
+    star_flag.value = true
+  } else {
+    star_num.value--
+    star_flag.value = false
+  }
 }
 
 onMounted(async () => {
@@ -29,7 +68,23 @@ onMounted(async () => {
 
 <template>
   <div>
-    <div class="mb-4"><span class="j-intro">元素说明</span></div>
+    <div class="mb-4">
+      <span class="j-intro">元素说明</span>
+      <span style="float: right">
+        <v-btn
+          color="#61666D"
+          icon="mdi-thumb-up"
+          variant="text"
+          @click="thumb(operate)"
+        ></v-btn>
+        <v-btn
+          color="#61666D"
+          icon="mdi-star"
+          variant="text"
+          @click="star(operate)"
+        ></v-btn>
+      </span>
+    </div>
     <div>{{ topicInfo.content }}</div>
   </div>
 </template>
